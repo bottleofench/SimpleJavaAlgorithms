604a44efbc09ddfee6ffa0f3a365b268662ee28d
bottleofench <71398923+bottleofench@users.noreply.github.com>
2022-06-03 21:55:14 +0600

    Some tweaks

--- src/api/bottleofench/main/Factorial.java
+++ src/api/bottleofench/main/Factorial.java
@@ -7,9 +7,9 @@ public class Factorial {
     public static void main(String[] args) {
         System.out.println("Введите число, факториал которого вы хотите получить:");
         Scanner scanner = new Scanner(System.in);
-        int number = scanner.nextInt();
-        int output = 1;
-        for (int i = 1; i <= number; i++) output *= i;
+        long number = scanner.nextLong();
+        long output = 1;
+        for (long i = 1; i <= number; i++) output *= i;
         System.out.println(output);
     }
 }
--- src/api/bottleofench/main/PrimeNumber.java
+++ src/api/bottleofench/main/PrimeNumber.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 
 public class PrimeNumber {
     public static void main(String[] args) {
-        System.out.println("Внимание: есть большая погрешность на проверке при больших числах!");
+        System.out.println("Внимание: присутствует большая погрешность на проверке при больших числах!");
         System.out.println("Введите число, которые вы хотите проверить на то, является ли оно простым:");
         Scanner scanner = new Scanner(System.in);
         long number = scanner.nextLong();
         boolean prime = true;
-        List<Long> simple_prime_numbers = List.of(2L, 3L, 5L, 7L, 11L, 13L, 17L, 19L);
+        List<Long> simple_prime_numbers = List.of(2L, 3L, 5L, 7L, 11L, 13L, 17L, 19L, 23L);
 
         if (!simple_prime_numbers.contains(number)) {
             for (long i : simple_prime_numbers) {
